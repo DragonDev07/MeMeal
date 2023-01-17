8475efd9576760d54bcf0efd23bca4b48fe3591f
A. Teo Welton <76081718+DragonDev07@users.noreply.github.com>
2023-01-17 08:48:38 -0700

    Update chatbot.py to match deta host file

--- src/chatbot.py
+++ src/chatbot.py
@@ -15,8 +15,8 @@ load_dotenv()
 openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 # Function to generate a recipe using the OpenAI API and inspiration from the edamam API
-def generate_recipe(dietary_restrictions, allergies, edamam_data):
-    prompt = f"generate a recipe that is {' and '.join(dietary_restrictions)} and does not include {' or '.join(allergies)}. Make sure to include the name of the recipe at the top. Also, you can use the following recipe as inspiration: {edamam_data['hits'][0]['recipe']['label']}"
+def generate_recipe(dietary_restrictions, allergies, query, user_input, edamam_data):
+    prompt = f"The full input from the user is: {user_input} generate a recipe that is {' and '.join(dietary_restrictions)} and does not include {' or '.join(allergies)}. It should also include {' and '.join(query)}Make sure to include the name of the recipe at the top. Also, you can use the following recipe as inspiration: {edamam_data['hits'][0]['recipe']['label']} in the text please dont include line breaks and instead do <br>"
     completions = openai.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
@@ -35,18 +35,30 @@ def handle_input(user_input):
     query = []
     for word in input_list:
         if word in ["alcohol-cocktail", "alcohol-free", "celery-free", "crustacean-free", "dairy-free", "DASH", "egg-free", "fish-free", "fodmap-free", "gluten-free", "immuno-supportive", "keto-friendly", "kidney-friendly", "kosher", "low-potassium", "low-sugar", "lupine-free", "Mediterranean", "mollusk-free", "mustard-free", "no-oil-added", "paleo", "peanut-free", "pescatarian", "pork-free", "red-meat-free", "sesame-free", "shellfish-free", "soy-free", "sugar-conscious", "sulfite-free", "tree-nut-free", "vegan", "vegetarian", "wheat-free"]:
-            dietary_restrictions.append(word)
+                dietary_restrictions.append(word)
         elif word in ["peanut", "tree-nut", "shellfish", "fish", "dairy", "egg", "soy", "wheat"]:
-            allergies.append(word)
-        elif word in ["chicken", "beef", "pork", "fish", "vegetables", "soup"]:
-            query.append(word)
+                allergies.append(word)
+        elif word in ["chicken", "beef", "pork", "fish", "vegetables", "soup", "asian", "chinese", "curry"]:
+                query.append(word)
     
-    edamam_url = f"https://api.edamam.com/api/recipes/v2?type=public&q={query[0]}&app_id={os.environ.get('EDAMAM_APP_ID')}&app_key={os.environ.get('EDAMAM_APP_KEY')}&health={dietary_restrictions[0]}&excluded={allergies[0]}"
+    if not dietary_restrictions:
+        dietary_restrictions = ["alcohol-free"]
+    if not allergies:
+        allergies = ["peanut"]
+    if not query:
+        query = ["chicken"]
+
+    edamam_app_id = os.environ.get('EDAMAM_APP_ID')
+    edamam_app_key = os.environ.get('EDAMAM_APP_KEY')
+
+    edamam_url = f"https://api.edamam.com/api/recipes/v2?type=public&q={query[0]}&app_id={edamam_app_id}&app_key={edamam_app_key}&health={dietary_restrictions[0]}&excluded={allergies[0]}"
+
+    print(edamam_url)
 
     edamam_response = requests.get(edamam_url)
     edamam_data = edamam_response.json()
 
-    recipe = generate_recipe(dietary_restrictions, allergies, edamam_data)
+    recipe = generate_recipe(dietary_restrictions, allergies, query, user_input, edamam_data)
     return recipe
 
 @app.route('/generate_recipe', methods=['POST'])
